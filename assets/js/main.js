e6dd9a70f0b5407186589765866c94a4b847aebf
Nico-Serra <nico.serra000@gmail.com>
2024-02-21 15:30:24 +0100

    add if with fizz and buzz

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,25 @@ const elementUl = document.querySelector('ul')
 
 //-Scrivere un programma utilizzando il ciclo for per prendere i numeri da 1 a 100
 for (let i=1; i <= 100; i++){
+    //-li stampo in console
     //console.log(i);
-    elementUl.insertAdjacentHTML('beforeend', `<li>${i}</li>`)
-}
 
-/*-li stampo in console
-    -SE la mia variabile i/3 scrivo Fizz
-    -ALTRIMENTI SE la mia variabile i/5 scrivo Buzz
-    -ALTRIMENTI SE la mia variabile i/5 AND i/3 scrivo FizzBuzz
-*/
+    
+     if (i % 3 === 0) {
+        //SE la mia variabile i/3 scrivo Fizz
+        //console.log(i);
+        elementUl.innerHTML += '<li>fizz</li>'
+     } else if (i % 5 === 0) {
+        //console.log(i);
+        elementUl.innerHTML += '<li>buzz</li>'
+     } else if (i % 3 === 0 && i % 5 === 0 ) {
+        console.log(i);
+        elementUl.innerHTML += '<li>fizzbuzz</li>'
+     }
+
+     elementUl.insertAdjacentHTML('beforeend', `<li>${i}</li>`)
+    
+//-ALTRIMENTI SE la mia variabile i/5 scrivo Buzz
+    //-ALTRIMENTI SE la mia variabile i/5 AND i/3 scrivo FizzBuzz
+
+}
