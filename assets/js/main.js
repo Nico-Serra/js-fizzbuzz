1337942b07d55a91978a4870753320624d4a2b9f
Nico-Serra <nico.serra000@gmail.com>
2024-02-21 17:11:34 +0100

    add bonus 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 //console.log('it works');
 
-const elementUl = document.querySelector('ul');
+//const elementUl = document.querySelector('ul');
+
+const container = document.querySelector('.container')
+const ul = document.createElement('ul')
+container.append(ul)
+
+
 
 //-Scrivere un programma utilizzando il ciclo for per prendere i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
@@ -10,20 +16,32 @@ for (let i = 1; i <= 100; i++) {
 
     if (i % 3 === 0 && i % 5 === 0) {
         //-SE la mia variabile i/5 AND i/3 scrivo FizzBuzz
-        console.log('fizzbuz');
+        //console.log('fizzbuz');
         //elementUl.innerHTML = '<li>fizzbuzz</li>'
+        const li = document.createElement('li')
+        ul.append(li)
+        li.append('fizzbuzz')
     } else if (i % 3 === 0) {
         //ALTRIMENTI SE la mia variabile i/3 scrivo Fizz
-        console.log('fizz');
+        //console.log('fizz');
         //elementUl.innerHTML += '<li>fizz</li>'
+        const li = document.createElement('li')
+        ul.append(li)
+        li.append('fizz')
         
     } else if (i % 5 === 0) {
         //-ALTRIMENTI SE la mia variabile i/5 scrivo Buzz
-        console.log('buzz');
-       // elementUl.innerHTML += '<li>buzz</li>'
+        //console.log('buzz');
+       //elementUl.innerHTML += '<li>buzz</li>'
+       const li = document.createElement('li')
+        ul.append(li)
+        li.append('buzz')
     } else {
-        console.log(i);
+        //console.log(i);
         //elementUl.insertAdjacentHTML('beforeend', `<li>${i}</li>`);
+        const li = document.createElement('li')
+        ul.append(li)
+        li.append(`${i}`)
     }
 
 };
